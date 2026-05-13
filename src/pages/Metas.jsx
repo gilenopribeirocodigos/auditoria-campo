ee257eef85115ef63496b44b50bb329c488d1f25
gilenopribeirocodigos <gilenopribeiro@gmail.com>
2026-05-13 16:15:53 -0300

    Update Metas.jsx

--- src/pages/Metas.jsx
+++ src/pages/Metas.jsx
@@ -3,7 +3,7 @@ import { supabase } from '../lib/supabase.js'
 
 function mesAtual() {
   const d = new Date()
-  return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}`
+  return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, '0')}` 
 }
 
 function mesLabel(mesAno) {
